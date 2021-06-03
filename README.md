caf6600d98a6543d96af3bdc418326f48477a253
tocchimittsu <arimagedon21@gmail.com>
2021-06-03 15:28:21 +0900

    修正

---
+++ README.md
@@ -0,0 +1,5 @@
+# tkinter-scrollbar
+Tkinterのscrollbarに関するコード
+
+# 参考記事
+https://kuroro.blog/python/vgx53M7D1d6C0R8ejp0V/
---
+++ main.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+
+# Windowを取得する。
+def getWindow(root):
+    # Windowをループさせて、継続的にWindow表示させる。
+    root.mainloop()
+
+# Frameを取得する。
+def getFrame(root):
+    # Windowを親要素として、frame Widget(Frame)を作成する。
+    # Frameについて : https://kuroro.blog/python/P20XOidA5nh583fYRvxf/
+    frame = tk.Frame(root)
+    # Frameをループさせて、継続的にFrame表示させる。
+    frame.mainloop()
+
+# Widgetを取得する。
+def getWidget(root):
+    # Windowを親要素として、frame Widget(Frame)を作成する。
+    # Frameについて : https://kuroro.blog/python/P20XOidA5nh583fYRvxf/
+    frame = tk.Frame(root)
+
+    # frame Widget(Frame)を親要素として、text Widgetを作成する。
+    text = tk.Text(frame)
+    # Widgetをループさせて、継続的にWidget表示させる。
+    text.mainloop()
+
+# 足し算を行う関数
+# 第一引数 : number
+# 第二引数 : number
+# 戻り値 : number
+def calcPlus(preNum, nextNum):
+    return preNum + nextNum
+
+# helloをloopする関数
+def getLoop():
+    while True:
+        print('hello')
+
+# Tkinter初学者参考 : https://docs.python.org/ja/3/library/tkinter.html#a-simple-hello-world-program
+if __name__ == "__main__":
+    root = tk.Tk()
+    # getWindow(root)
+    # getFrame(root)
+    # getWidget(root)
+    # print(calcPlus(1, 5))
+    # getLoop()
